4c423ae2c4f40e80c11bcb9573bbe573d60c93da
Armela Ligori <aligori@rpay.ai>
2023-06-05 13:51:49 +0200

    Removed Bind Mount

---
+++ mysql/script.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test (
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email VARCHAR(255)
+);
--- server/services/mysql/services.js
+++ server/services/mysql/services.js
@@ -1,14 +1,14 @@
 import db from './db.service.js'
 
 async function createTest(data){
-    const query = 'INSERT INTO emails (firstname, lastname, email) VALUES (?, ?, ?)';
+    const query = 'INSERT INTO test (firstname, lastname, email) VALUES (?, ?, ?)';
     const params = [data.firstname, data.lastname, data.email]
     
     return await db.executeQuery(query, params);
 }
 
 async function getTest(){
-    const query = 'SELECT * FROM emails';
+    const query = 'SELECT * FROM test';
     return await db.executeQuery(query);
 }
 
