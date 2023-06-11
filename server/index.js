6024cc76778efa2657eb686ffbe58c47414af2c9
Armela Ligori <aligori@rpay.ai>
2023-06-11 02:24:05 +0200

    Generated data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express, { urlencoded, json } from 'express';
+import express, {urlencoded, json} from 'express';
 import * as dotenv from 'dotenv'
 import mysqlServices from './services/mysql/services.js'
 import mongoServices from './services/mongo/services.js'
@@ -9,50 +9,104 @@ const app = express();
 
 // Using MySql services by default
 let services = mysqlServices
-  
+
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
 app.use((req, res, next) => {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
- });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // JUST FOR TESTING - NEED TO BE REMOVED
 app.post('/test', async (req, res) => {
-   try {
-     const results = await services.createTest(req.body)
-     res.status(200).send({message: 'Success'});
-   } catch (error) {
-     res.status(500).send({ error });
-   }
+  try {
+    const results = await services.createTest(req.body)
+    res.status(200).send({ message: 'Success' });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 
 app.get('/test', async (req, res) => {
-   try {
-      const results = await services.getTest()
-      res.status(200).send({ message: 'Fetch success', data: results });
-   } catch (err) {
-      res.status(500).send('Error fetching!');
-   }
+  try {
+    const results = await services.getTest()
+    res.status(200).send({ message: 'Fetch success', data: results });
+  } catch (err) {
+    res.status(500).send('Error fetching!');
+  }
 })
+
+app.post('/fill-database', async (req, res) => {
+  try {
+    const result = await services.fillDatabase()
+    res.status(200).send({ message: result });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
 
 app.post('/migrate', async (req, res) => {
-   try {
-      // Connect to mongo db
-      connectToMongo()
-      // Call a migrate function that converts rdbms data to nosql data
-      // Change to services that use mongo
-      services = mongoServices
-      res.status(200).send('Successfully Migrated!');
-   } catch (err) {
-      res.status(500).send('Error fetching!');
-   }
+  try {
+    // Connect to mongo db
+    await connectToMongo()
+    // Call a migrate function that converts rdbms data to nosql data
+    // Change to services that use mongo
+    services = mongoServices
+    res.status(200).send('Successfully Migrated!');
+  } catch (err) {
+    res.status(500).send('Internal server error!');
+  }
 })
-  
+
+app.get('/users', async (req, res, next) => {
+    try {
+      let type = req.query.type;
+      if (!type || !['goer', 'influencer'].includes(type)) {
+        return res.status(400).send({ error: 'Bad request!' });
+      }
+      const data = type === 'goer' ? await services.getGymGoers() : await services.getInfluencers()
+      res.status(200).send({
+        message: 'Success', data: data.map(user => {
+          return { id: user.id, username: user.username, email: user.email, avatar: user.avatar }
+        })
+      });
+    } catch
+      (err) {
+      res.status(500).send({ error: 'Internal server error' });
+    }
+  }
+)
+
+app.get('/categories', async (req, res) => {
+    try {
+      const data = await services.getCategories();
+      res.status(200).send({
+        message: 'Success', data
+      });
+    } catch
+      (err) {
+      res.status(500).send({ error: 'Internal server error' });
+    }
+  }
+)
+
+app.get('/plans', async (req, res) => {
+    try {
+      const data = await services.getPlans();
+      res.status(200).send({
+        message: 'Success', data
+      });
+    } catch
+      (err) {
+      res.status(500).send({ error: 'Internal server error' });
+    }
+  }
+)
+
 const PORT = process.env.SERVER_PORT || 8080;
 
-app.listen(PORT, 
-   () => console.log(`Server is running at port: ${PORT}`));
+app.listen(PORT,
+  () => console.log(`Server is running at port: ${PORT}`));
--- server/services/mysql/services.js
+++ server/services/mysql/services.js
@@ -1,15 +1,145 @@
 import db from './db.service.js'
+import { faker } from '@faker-js/faker'
+import {getRandomInt} from "../../utils/helpers.js";
+import {defaultCategories} from "../../data/categories.js";
+import {defaultExercises} from "../../data/exercises.js";
 
-async function createTest(data){
+async function createTest(data) {
     const query = 'INSERT INTO test (firstname, lastname, email) VALUES (?, ?, ?)';
     const params = [data.firstname, data.lastname, data.email]
-    
+
     return await db.executeQuery(query, params);
 }
 
-async function getTest(){
+async function getTest() {
     const query = 'SELECT * FROM test';
     return await db.executeQuery(query);
 }
 
-export default { createTest, getTest }
+async function fillDatabase() {
+    // Creating Gym Goer Users
+    const gymGoerUsers = Array.from({ length: 100 }, () => ([
+        faker.internet.email(),
+        faker.internet.password(),
+        faker.internet.userName(),
+        faker.internet.avatar(),
+    ]));
+
+    const gymGoerIds = Array.from({length: 100}, (_, i) => i + 1);
+    const gymGoers = gymGoerIds.map((id) => [
+        id,
+        parseFloat(faker.number.float({ min: 30, max: 150, precision: 0.1 })),            // Weight
+        parseFloat(faker.number.float({ min: 140, max: 230, precision: 0.1 })),           // Height
+        faker.date.past({ years: 60 }),                                                   // Birthday
+        faker.helpers.arrayElement(['free', 'basic', 'silver', 'gold'])                     // Membership Type
+    ])
+
+    await db.executeQuery('INSERT INTO user (email, password, username, avatar) VALUES ?', [gymGoerUsers])
+    await db.executeQuery('INSERT INTO gym_goer (goer_id, weight, height, birthday, membership_type) VALUES ?', [gymGoers])
+
+    // Creating Fitness Influencer Users
+    const influencerUsers = Array.from({ length: 20 }, () => ([
+        faker.internet.email(),
+        faker.internet.password(),
+        faker.internet.userName(),
+        faker.internet.avatar(),
+    ]));
+
+    const influencerIds = Array.from({length: 20}, (_, i) => i + 100);
+    const influencers = influencerIds.map((id) => [
+        id,
+        faker.person.firstName(),
+        faker.person.lastName(),
+        getRandomInt(1, 20),
+        faker.internet.avatar(),
+        faker.internet.url(),
+        faker.lorem.text()
+    ])
+
+    await db.executeQuery('INSERT INTO user (email, password, username, avatar) VALUES ?', [influencerUsers])
+    await db.executeQuery('INSERT INTO fitness_influencer (influencer_id, first_name, last_name, experience, photo, website, bio) VALUES ?', [influencers])
+
+    // Creating Categories
+    const categories = defaultCategories.map((c) => [c.name, c.description, faker.image.urlLoremFlickr()])
+    await db.executeQuery('INSERT INTO category (name, description, image) VALUES ?', [categories])
+
+    // Creating Exercises
+    const exercises = defaultExercises.map((e) => [e.name, e.description, e.equipment, e.calories_burned, JSON.stringify(e.muscle_groups), faker.image.urlLoremFlickr()])
+    await db.executeQuery('INSERT INTO exercise (name, description, equipment, calories_burned, muscle_groups, image) VALUES ?', [exercises])
+
+    for (let id = 1; id <= categories.length; id++) {
+        const numberOfPlansInCategory = getRandomInt(3, 15)
+
+        for (let i = 1; i <= numberOfPlansInCategory; i++) {
+            const planDuration = getRandomInt(7, 30);
+            const influencerId = getRandomInt(100, 119);
+
+            const plan = {
+                title: faker.lorem.words(),
+                description: faker.lorem.paragraph(),
+                goal: faker.lorem.words(),
+                duration: planDuration,
+                createdAt: faker.date.recent(),
+                influencerId,
+                categoryId: id,
+            }
+
+            console.log('plan', plan)
+
+            const planQuery = 'INSERT INTO plan (title, description, goal, duration, created_at, influencer_id, category_id) VALUES (?, ?, ?, ?, ?, ?, ?)'
+            const params = [plan.title, plan.description, plan.goal, plan.duration, plan.createdAt, plan.influencerId, plan.categoryId]
+            await db.executeQuery(planQuery, params)
+
+            console.log('hereeee')
+
+            const [records] = await db.executeQuery('SELECT LAST_INSERT_ID() as planId From plan;')
+            const { planId } = records
+            console.log('plan id', planId)
+
+            const workouts = [];
+
+            for (let day = 1; day <= planDuration; day++) {
+                // Create workouts
+                const workout = [
+                    faker.lorem.words(),
+                    faker.helpers.arrayElement(['easy', 'medium', 'hard']),
+                    getRandomInt(30, 120),
+                    faker.image.url(),
+                    day,
+                    planId,
+                    influencerId,
+                ]
+                workouts.push(workout)
+            }
+
+            await db.executeQuery('INSERT INTO workout (title, difficulty, duration, image, scheduled_day, plan_id, influencer_id) VALUES ?', [workouts])
+        }
+    }
+
+    // TODO: FIX THIS: MongoServerError: $jsonSchema keyword 'required' must be an array, but found an element of type bool
+    // fitness-app-mongo  | {"t":{"$date":"2023-06-09T19:03:06.112+00:00"},"s":"I",  "c":"NETWORK",  "id":22944,   "ctx":"conn12","msg":"Connection ended","att
+    // r":{"remote":"127.0.0.1:47518","uuid":"626451f2-23a4-4cf8-9fa3-b3dfb79cc7b2","connectionId":12,"connectionCount":3}}
+
+
+    return 'Database filled!'
+}
+
+async function getGymGoers() {
+    const query = 'SELECT * FROM user as u inner join gym_goer as gg on u.id = gg.goer_id';
+    return await db.executeQuery(query);
+}
+
+async function getInfluencers() {
+    const query = 'SELECT * FROM user as u inner join fitness_influencer as f on u.id = f.influencer_id';
+    return await db.executeQuery(query);
+}
+
+async function getCategories() {
+    return await db.executeQuery('SELECT * FROM category');
+}
+
+async function getPlans() {
+    return await db.executeQuery('SELECT * FROM plan');
+}
+
+export default { createTest, getTest, getGymGoers, getInfluencers, getPlans, getCategories, fillDatabase }
